c0db1a461d787c7b48d950bf7a49bef6a1638710
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2021-12-15 11:53:45 +0700

    fix bugs login + register page

--- HornSneaker/src/main/resources/static/assets/js/login.js
+++ HornSneaker/src/main/resources/static/assets/js/login.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const login = document.querySelector("#login-form");
-    const signup = document.querySelector("#signup-form");
-
-    document.querySelector(".link-to-signup-form").addEventListener("click",e =>{
-        e.preventDefault();
-        login.classList.remove("show-form");
-        signup.classList.add("show-form");
-    })
-
-    document.querySelector(".link-to-login-form").addEventListener("click",e =>{
-        e.preventDefault();
-        signup.classList.remove("show-form");
-        login.classList.add("show-form");
-    })
-});
 
 var header = document.getElementById('header')
 var resMenu = document.getElementById('res-menu-btn')
@@ -28,3 +12,17 @@ resMenu.onclick = function(){
         header.style.height = '50px';
     }
 }
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm-password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
