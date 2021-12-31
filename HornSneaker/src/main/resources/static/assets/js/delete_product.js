18e0b17adbafae6a6559534211bcbf9631b06886
NguyenQuocThong2603 <63840452+NguyenQuocThong2603@users.noreply.github.com>
2022-01-01 00:24:24 +0700

    Update edit page and fix delete button

--- HornSneaker/src/main/resources/static/assets/js/delete_product.js
+++ HornSneaker/src/main/resources/static/assets/js/delete_product.js
@@ -1,17 +1,17 @@
-var btnOpen = document.querySelector('.delete-btn')
-var modal = document.querySelector('.modal-delete')
-var iconClose = document.querySelector('.modal-delete-header i')
-var btnClose = document.querySelector('.cancel-btn')
+var btnOpen = document.querySelector('.product_delete-btn');
+var modal = document.querySelector('.modal-delete');
+var iconClose = document.querySelector('.modal-delete-header i');
+var btnClose = document.querySelector('.cancel-btn');
 
 function toggleModal(e){
     console.log(e.target);
-    modal.classList.toggle('hide')
+    modal.classList.toggle('hide');
 }
 btnOpen.addEventListener('click',toggleModal);
 btnClose.addEventListener('click',toggleModal);
 iconClose.addEventListener('click',toggleModal);
 modal.addEventListener('click',function(e){
-    if(e.target==e.currentTarget){
+    if(e.target===e.currentTarget){
         toggleModal();
     }
 });
